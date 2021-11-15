7b0c9d6d4cabbc62fb586ce4646713dd3dd58fc5
KunigiMohan <kmohanr5@gmail.com>
2021-11-15 10:02:43 +0530

    Added : Testing TC 1.2 & TC 1.2.

--- src/MoodAnalyserTest.java
+++ src/MoodAnalyserTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyserTest {
@@ -6,6 +8,19 @@ class MoodAnalyserTest {
     void analyseMood() {
         MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
         obj.analyseMood("SAD");
-        assertEquals("SAD",obj.analyseMood("sad"));     //Checking input value matches with given condition
+        assertEquals("SAD",obj.analyseMood("Sad"));     //Checking input value matches with given condition
+    }
+    @org.junit.jupiter.api.Test                                       //Testing TC 1.1
+    void analyseMood2() {
+        MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
+        obj.analyseMood("SAD");
+        assertEquals("SAD",obj.analyseMood("I am in Sad Mood"));     //Checking input value matches with given condition
+    }
+
+    @org.junit.jupiter.api.Test
+    void analyseMood3() {                                       //Testing TC1.2
+        MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
+
+        assertEquals("HAPPY",obj.analyseMood("I am in Any Happy Mood"));
     }
 }
