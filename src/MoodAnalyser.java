bd793d1ebd55b646270fbbff17b9202a385df921
KunigiMohan <kmohanr5@gmail.com>
2021-11-15 11:38:45 +0530

    Added : Modified Test Class to throw NullPointException to method.

--- src/MoodAnalyser.java
+++ src/MoodAnalyser.java
@@ -3,25 +3,25 @@ import java.util.Locale;
 public class MoodAnalyser {
     String message;
 
-
-
     MoodAnalyser(String message){
         this.message=message;
 
     }
+        public String analyseMood () {
+            MoodAnalyser obj = new MoodAnalyser(message);
 
-    public String analyseMood(){
-        MoodAnalyser obj = new MoodAnalyser(message);
-        String word = message.toLowerCase(Locale.ROOT);
-        boolean store = obj.message.toLowerCase(Locale.ROOT).contains("sad");
-
-        if(store == true)                   //Checking input contain sad or not
-        {
-            return "SAD";
+            try {                                               //Handling Exception using try catch
+                boolean store = obj.message.toLowerCase(Locale.ROOT).contains("sad");
+                if (store == true)                   //Checking input contain sad or not
+                {
+                    return "SAD";
+                } else {
+                    return "HAPPY";
+                }
+            }
+            catch (Exception rv){           //Catching of exception and return Happy
+                System.out.println("Enter Proper Value ");
+                return "Happy";
+            }
         }
-        else
-        {
-            return "HAPPY";
-        }
-    }
 }
