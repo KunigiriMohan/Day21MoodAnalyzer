598a00e23b2060e878e2e8f2482a559c4d0a642d
KunigiMohan <kmohanr5@gmail.com>
2021-11-15 10:49:34 +0530

    Added : Refactor the test class According to send arguments to constructer.

--- src/MoodAnalyserTest.java
+++ src/MoodAnalyserTest.java
@@ -6,21 +6,19 @@ class MoodAnalyserTest {
 
     @org.junit.jupiter.api.Test
     void analyseMood() {
-        MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
-        obj.analyseMood("SAD");
-        assertEquals("SAD",obj.analyseMood("Sad"));     //Checking input value matches with given condition
+        MoodAnalyser obj1= new MoodAnalyser("sad");
+        assertEquals("SAD",obj1.analyseMood());     //Checking input value matches with given condition
     }
     @org.junit.jupiter.api.Test                                       //Testing TC 1.1
     void analyseMood2() {
-        MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
-        obj.analyseMood("SAD");
-        assertEquals("SAD",obj.analyseMood("I am in Sad Mood"));     //Checking input value matches with given condition
+        MoodAnalyser obj = new MoodAnalyser("I am in Sad Mood"); //Creating MoodAnalyser object to access method in MoodAnalyser Class
+        assertEquals("SAD",obj.analyseMood());                  //Checking input value matches with given condition
     }
 
     @org.junit.jupiter.api.Test
     void analyseMood3() {                                       //Testing TC1.2
-        MoodAnalyser obj = new MoodAnalyser();              //Creating MoodAnalyser object to access method in MoodAnalyser Class
+        MoodAnalyser obj = new MoodAnalyser("I am in Any Happy Mood");              //Creating MoodAnalyser object to access method in MoodAnalyser Class
 
-        assertEquals("HAPPY",obj.analyseMood("I am in Any Happy Mood"));
+        assertEquals("HAPPY",obj.analyseMood());
     }
 }
