a674b913208f2d83b67b0795122109db2e3e4c42
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-11-15 11:01:36 +0530

    Added : Refactor the code to Taking inputs through Constructer.

--- src/MoodAnalyser.java
+++ src/MoodAnalyser.java
@@ -2,18 +2,13 @@ import java.util.Locale;
 
 public class MoodAnalyser {
     String message;
-
-
-
     MoodAnalyser(String message){
-        this.message=message;
-
+        this.message=message;                               //Creating constructer of the class
     }
 
     public String analyseMood(){
-        MoodAnalyser obj = new MoodAnalyser(message);
-        String word = message.toLowerCase(Locale.ROOT);
-        boolean store = obj.message.toLowerCase(Locale.ROOT).contains("sad");
+        MoodAnalyser obj = new MoodAnalyser(message);                       
+        boolean store = obj.message.toLowerCase(Locale.ROOT).contains("sad");       //Checking Word present in string by converting into lowerCase
 
         if(store == true)                   //Checking input contain sad or not
         {
